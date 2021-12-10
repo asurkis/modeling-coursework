f9467c62c1b478d738defc77cc84d50f2c2f7cc7
Anton Surkis <antonsurkis@gmail.com>
2021-12-10 21:24:16 +0300

    Add basic checkboxes and result grid

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 import wx.grid
-
 import approximations
 
+
+def update_sample_count(evt):
+    print(f'update sample count: {evt.Int}')
+
+
+def update_plots(evt):
+    print(f'update bucket count: {evt.Int}')
+
+
 app = wx.App()
-frame = wx.Frame(None, title='Проверка лабораторной № 1')
+settings_frame = wx.Frame(None, title='Проверка лабораторной 1')
+plot_frame = wx.MiniFrame(settings_frame, title='Настройки', style=wx.CAPTION)
+table_frame = wx.MiniFrame(settings_frame, title='Статистические параметры', style=wx.CAPTION)
 
-checkbox_frame = wx.MiniFrame(frame, title='Выбор приближений', style=wx.CAPTION)
+settings_sizer = wx.GridBagSizer(2, hgap=10)
+sample_count_ctrl = wx.SpinCtrl(settings_frame, min=1, max=100_000, style=wx.EXPAND)
+bucket_count_ctrl = wx.SpinCtrl(settings_frame, min=1, max=1000, style=wx.EXPAND)
+settings_sizer.Add(sample_count_ctrl, (0, 0), flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL)
+settings_sizer.Add(bucket_count_ctrl, (1, 0), flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL)
+settings_sizer.Add(wx.StaticText(settings_frame, label='Количество проб'), (0, 1), flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL)
+settings_sizer.Add(wx.StaticText(settings_frame, label='Размер гистограммы'), (1, 1), flag=wx.EXPAND | wx.ALIGN_CENTER_VERTICAL)
 
-approximation_checkboxes = wx.BoxSizer(wx.VERTICAL)
-for key in approximations.casual:
-    checkbox = wx.CheckBox(checkbox_frame, label=key)
+sample_count_ctrl.Bind(wx.EVT_SPINCTRL, update_sample_count)
+bucket_count_ctrl.Bind(wx.EVT_SPINCTRL, update_plots)
+
+for i, key in enumerate(approximations.casual):
+    checkbox = wx.CheckBox(settings_frame, label=key)
     evt = wx.CommandEvent()
     evt.GetClientObject()
     checkbox.Bind(wx.EVT_CHECKBOX, lambda x: print(f'Triggered {key} ({x.IsChecked()})'))
-    approximation_checkboxes.Add(checkbox)
+    settings_sizer.Add(checkbox, (i + 2, 0), (1, 2), flag=wx.EXPAND)
 
-approximation_checkboxes.Fit(checkbox_frame)
-checkbox_frame.SetSizer(approximation_checkboxes)
-checkbox_frame.Show()
+settings_sizer.Fit(settings_frame)
+settings_frame.Sizer = settings_sizer
 
-table_frame = wx.MiniFrame(frame, title='Статистические параметры', style=wx.CAPTION)
 table = wx.grid.Grid(table_frame)
 table.EnableEditing(False)
 table.CreateGrid(5, 2)
 table.SetColLabelValue(1, 'col label')
 table.SetRowLabelValue(2, 'row label')
-table_frame.Show()
 
-frame.Show()
+# table_frame.Show()
+# plot_frame.Show()
+settings_frame.Show()
 app.MainLoop()
